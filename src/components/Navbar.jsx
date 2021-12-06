cd83f3c4687bcea6a93f130e5a59332c813998f7
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-12-06 20:49:32 -0300

    animate mobileNavbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import * as styles from "../styles/Navbar.module.css"
 import { Link } from "gatsby"
+import { motion, AnimatePresence } from "framer-motion"
 
 // Icons
 import PTFlag from "../vectors/PTFlag.jsx"
@@ -41,56 +42,85 @@ const DesktopNavbar = () => (
   </nav>
 )
 
+const menuVariants = {
+  hidden: {
+    opacity: 0,
+    x: 500,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.2, ease: "easeInOut" },
+  },
+}
+
 const MobileNavbar = () => {
   const [open, setOpen] = useState(false)
 
-  return open ? (
-    <nav className={styles.mobileNav}>
-      <button className={styles.closeIcon} onClick={e => setOpen(false)}>
-        <CloseIcon />
-      </button>
-      <h1>Menu:</h1>
-      <Link className={styles.mobileItem} to="/">
-        Home
-      </Link>
-      <Link className={styles.mobileItem} to="/produtos">
-        Produtos
-      </Link>
-      <Link className={styles.mobileItem} to="contato">
-        Contato
-      </Link>
-      <Link className={styles.mobileItem} to="#">
-        Endereço
-      </Link>
-      <div className={styles.mobileLanguageSelector}>
-        <h2>IDIOMA:</h2>
-        <Link to="#" className={styles.mobileLink}>
-          <PTFlag className={styles.mobileIcons} />
-          Português
-        </Link>
-        <Link to="#" className={styles.mobileLink}>
-          <USFlag className={styles.mobileIcons} />
-          English
-        </Link>
-        <Link to="#" className={styles.mobileLink}>
-          <SPFlag className={styles.mobileIcons} />
-          Español
-        </Link>
-      </div>
-    </nav>
-  ) : (
-    <button
-      className={styles.burguerButton}
-      onClick={e => setOpen(prev => !prev)}
-    >
-      <MenuIcon />
-    </button>
+  return (
+    <AnimatePresence exitBeforeEnter>
+      {open ? (
+        <motion.nav
+          className={styles.mobileNav}
+          key="1"
+          variants={menuVariants}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+        >
+          <button className={styles.closeIcon} onClick={e => setOpen(false)}>
+            <CloseIcon />
+          </button>
+          <h1>Menu:</h1>
+          <Link className={styles.mobileItem} to="/">
+            Home
+          </Link>
+          <Link className={styles.mobileItem} to="/produtos">
+            Produtos
+          </Link>
+          <Link className={styles.mobileItem} to="contato">
+            Contato
+          </Link>
+          <Link className={styles.mobileItem} to="#">
+            Endereço
+          </Link>
+          <div className={styles.mobileLanguageSelector}>
+            <h2>IDIOMA:</h2>
+            <Link to="#" className={styles.mobileLink}>
+              <PTFlag className={styles.mobileIcons} />
+              Português
+            </Link>
+            <Link to="#" className={styles.mobileLink}>
+              <USFlag className={styles.mobileIcons} />
+              English
+            </Link>
+            <Link to="#" className={styles.mobileLink}>
+              <SPFlag className={styles.mobileIcons} />
+              Español
+            </Link>
+          </div>
+        </motion.nav>
+      ) : (
+        <motion.button
+          variants={menuVariants}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          key="2"
+          className={styles.burguerButton}
+          onClick={e => setOpen(prev => !prev)}
+        >
+          <MenuIcon />
+        </motion.button>
+      )}
+    </AnimatePresence>
   )
 }
 
 const Navbar = () => (
   <>
-    <MobileNavbar /> <DesktopNavbar />
+    <MobileNavbar />
+    <DesktopNavbar />
   </>
 )
 
