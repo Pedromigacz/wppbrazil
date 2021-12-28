f91e56e5e7f59f834453ed4e6d314f9619333c32
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2021-12-28 01:38:47 -0300

    resize navbar height

--- src/vectors/MainLogo.jsx
+++ src/vectors/MainLogo.jsx
@@ -4,8 +4,7 @@ function Icon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="193"
-      height="162"
+      height="100%"
       fill="none"
       viewBox="0 0 193 162"
     >
